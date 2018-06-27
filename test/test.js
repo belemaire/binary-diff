4059405cdd66e68106749774a16109bd7f20e26e
Benoit Lemaire <blemaire@walmartlabs.com>
2018-06-27 16:55:58 -0700

    Add test for diffBuffer

--- test/test.js
+++ test/test.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
 const path = require('path');
 const bsdiff = require('../index');
+const chai = require('chai');
+const chaiFiles = require('chai-files');
+chai.use(chaiFiles);
+const expect = chai.expect;
+const file = chaiFiles.file;
 
 describe('bsdiff', () => {
   describe('#diff() + #patch()', () => {
@@ -13,4 +19,20 @@ describe('bsdiff', () => {
       bsdiff.patch(oldFile, generatedFile, patchFile);
     });
   });
+
+  describe('#diffBuffer()', () => {
+    it('should produce identical output as diff', () => {
+      const oldFile = path.join(__dirname, 'resources/react-0.3-stable.zip');
+      const newFile = path.join(__dirname, 'resources/react-0.4-stable.zip');
+      const patchFile = path.join(__dirname, 'resources/react.patch');
+      bsdiff.diff(oldFile, newFile, patchFile);
+
+      const oldBuffer = fs.readFileSync(oldFile); 
+      const newBuffer = fs.readFileSync(newFile);
+      const patchBufferFile = path.join(__dirname, 'resources/react.buffer.patch');
+      bsdiff.diffBuffer(oldBuffer, oldBuffer.length, newBuffer, newBuffer.length, patchBufferFile);
+
+      expect(file(patchFile)).to.equal(file(patchBufferFile));
+    })
+  })
 });
